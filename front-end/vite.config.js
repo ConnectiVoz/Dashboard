df71b91b974dcc74ae9e7e59ebfee6e7344a0aee
root <root@ip-172-31-32-138.ec2.internal>
2025-08-06 08:32:10 +0000

    working copy

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: [
+       'ivozagent.rivoz.in','ec2-18-207-177-29.compute-1.amazonaws.com'
+   ],
     proxy: {
       '/api': {
         target: 'https://3.95.238.222',
